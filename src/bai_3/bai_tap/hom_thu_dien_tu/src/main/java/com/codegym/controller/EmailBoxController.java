bbb559b97038ecf5b884edf3ef790f1da0558783
huutrungu <huutrungg02@gmail.com>
2022-05-06 07:27:32 +0700

    fix comment

--- src/bai_3/bai_tap/hom_thu_dien_tu/src/main/java/com/codegym/controller/EmailBoxController.java
+++ src/bai_3/bai_tap/hom_thu_dien_tu/src/main/java/com/codegym/controller/EmailBoxController.java
@@ -1,6 +1,8 @@
 package com.codegym.controller;
 
 import com.codegym.model.EmailBox;
+import com.codegym.service.IEmailBoxService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class EmailBoxController {
+    @Autowired
+    IEmailBoxService iEmailBoxService;
 
     @GetMapping(value = "/home")
     public String home(Model model) {
 
-        model.addAttribute("emailBox",new EmailBox());
+        model.addAttribute("emailBox", new EmailBox());
 
         return "home";
     }
@@ -23,9 +27,8 @@ public class EmailBoxController {
     @PostMapping(value = "update")
     public String updateEmailBox(@ModelAttribute EmailBox emailBox,
                                  RedirectAttributes redirectAttributes) {
-
+        iEmailBoxService.update(emailBox);
         return "home";
-
     }
 
 }
