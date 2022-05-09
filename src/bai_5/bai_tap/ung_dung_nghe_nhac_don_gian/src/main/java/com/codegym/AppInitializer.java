c51b2a66e47ce1a0aaadf59a721d3192523f9d0c
huutrungu <huutrungg02@gmail.com>
2022-05-09 21:49:42 +0700

    update ss5

--- src/bai_5/bai_tap/ung_dung_nghe_nhac_don_gian/src/main/java/com/codegym/AppInitializer.java
+++ src/bai_5/bai_tap/ung_dung_nghe_nhac_don_gian/src/main/java/com/codegym/AppInitializer.java
@@ -20,10 +20,10 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
-//    @Override
-//    protected Filter[] getServletFilters() {
-//        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-//        characterEncodingFilter.setEncoding("UTF-8");
-//        return new Filter[] { characterEncodingFilter};
-//    }
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return new Filter[] { characterEncodingFilter};
+    }
 }
