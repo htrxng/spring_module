68aba97f3d9a60b42beb913f7ef932a2258085c0
huutrungu <huutrungg02@gmail.com>
2022-05-20 10:47:24 +0700

    fix cmt

--- src/bai_12/thuc_hanh/phone-management/src/main/java/com/codegym/controller/SmartphoneController.java
+++ src/bai_12/thuc_hanh/phone-management/src/main/java/com/codegym/controller/SmartphoneController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/smartphones")
 public class SmartphoneController {
@@ -22,6 +24,19 @@ public class SmartphoneController {
         ModelAndView modelAndView = new ModelAndView("/phones/list");
         modelAndView.addObject("smartphones", smartphoneService.findAll());
         return modelAndView;
+    }
+    @GetMapping
+    public ResponseEntity<Iterable<Smartphone>> allPhones() {
+        return new ResponseEntity<>(smartphoneService.findAll(), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Smartphone> deleteSmartphone(@PathVariable Long id) {
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        smartphoneService.remove(id);
+        return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
     }
 
 }
